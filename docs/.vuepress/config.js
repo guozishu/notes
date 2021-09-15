e9af6d5dd40a5fb1323699c8c1e00a8c3fc017f0
longwang <longwang@paypal.com>
2021-09-15 22:34:26 +0800

    nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,26 @@
 module.exports = {
     title: '技术博客',
-    description: '王海东技术博客'
+    description: '王海东技术博客',
+    themeConfig: {
+        logo: '/home-logo.png',
+        nav: [
+            { text: 'JavaScript', link: '/' },
+            { text: 'NodeJs', link: '/nodejs/' },
+            { text: 'GitHub', link: 'https://github.com/' },
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { text: 'Chinese', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            }
+
+        ],
+        sidebar: [
+            '/',
+            '/nodejs'
+          ]
+    },
+
 }
